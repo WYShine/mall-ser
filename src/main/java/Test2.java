fbec7c49208f1553b21363ca69aa19bf64792307
wx <wx@wxdeMacBook-Pro.local>
2021-04-28 08:44:02 +0800

    prove

--- src/main/java/Test2.java
+++ src/main/java/Test2.java
@@ -1,68 +1,32 @@
-import java.util.ArrayList;
-import java.util.List;
 
 public class Test2 {
-    static List<Integer> res = new ArrayList<>();
-    static boolean canJump = true;
-    static List<Integer> moveTrace(int target) {
-        res.add(0, target);
-        if (target == 0) {
-            return res;
-        }
-        if (target % 2 == 0 && canJump) {
-            canJump = false;
-            moveTrace(target / 2);
-        } else {
-            canJump = true;
-            moveTrace(target - 1);
-        }
-        return res;
-    }
-
-    static List<Integer> moveTrace1(int target) {
-//        System.out.println(target);
-        res.add(0, target);
-        if (target == 0) {
-            return res;
-        }
-        if (target == 1) {
-            res.add(0, 0);
-            return res;
-        }
-        if (target % 2 == 0) {
-            moveTrace1(target / 2);
-        } else {
-            List<Integer> list1 = new ArrayList<>(moveTrace1(target + 1));
-            int index = list1.indexOf(target + 1);
-            res = res.subList(index + 1, res.size());
-            moveTrace1(target - 1);
-            if (list1.size() < res.size()) {
-
-                res = list1;
+    static String minSubNum(String origin, int m) {
+        int length = origin.length();
+        StringBuilder sb = new StringBuilder();
+        int toRemainCount = length - m;
+        int leftBoard = -1;
+        while (toRemainCount > 0) {
+            int min = 10;
+            int tmpLeftBoard = leftBoard;
+            for (int i = length - toRemainCount; i > leftBoard; i--) {
+                int charAtNum = origin.charAt(i) - '0';
+                if (charAtNum > min) {
+                    continue;
+                }
+                if (toRemainCount != length - m || charAtNum != 0) {
+                    min = charAtNum;
+                    tmpLeftBoard = i;
+                }
             }
+            leftBoard = tmpLeftBoard;
+            sb.append(min);
+            toRemainCount--;
         }
-        return res;
+        return sb.toString();
     }
 
     public static void main(String[] args) {
-        System.out.println(moveTrace1(0));
-        res = new ArrayList<>();
-        System.out.println(moveTrace1(1));
-        res = new ArrayList<>();
-        System.out.println(moveTrace1(2));
-        res = new ArrayList<>();
-        System.out.println(moveTrace1(3));
-        res = new ArrayList<>();
-        System.out.println(moveTrace1(5));
-
-        res = new ArrayList<>();
-        System.out.println(moveTrace1(8));
-        res = new ArrayList<>();
-        System.out.println(moveTrace1(15));
-        res = new ArrayList<>();
-        System.out.println(moveTrace1(20));
-
-        res = new ArrayList<>();
-        System.out.println(moveTrace1(23));
+        System.out.println(minSubNum("23198", 2));
+        System.out.println(minSubNum("123198", 2));
     }
 }
